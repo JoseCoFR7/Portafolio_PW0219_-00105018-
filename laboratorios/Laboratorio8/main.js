e583fbdc7eff07094e40dd6d0a7a88d47f9bc28d
JoseCoFR7 <00105018@uca.edu.sv>
2019-10-19 21:37:16 -0600

    [Labo 08] Finalizado

--- laboratorios/Laboratorio8/main.js
+++ laboratorios/Laboratorio8/main.js
@@ -1,111 +1,140 @@
-let carnet_field =  document.querySelector("#carnet_field")
-let schedule_dropdown =  document.querySelector("#schedule_field")
-let late_switch =  document.querySelector("#late_switch")
-let submit_btn =  document.querySelector("#submit_btn")
+let carnet_field = document.querySelector("#carnet_field")
+let schedule_dropdown = document.querySelector("#schedule_field")
+let late_switch = document.querySelector("#late_switch")
+let submit_btn = document.querySelector("#submit_btn")
 
 let table_body = document.querySelector("#table_body")
 let carnet_regex= new RegExp('^[0-9]{8}$')
 
-let student_list = [];
-let serial =0;
+let student_list = []
+let serial = 0
 
+let printArray = ()=>{
+table_body.innerHTML=""
+
+student_list.forEach(element =>{
+let new_row = document.createElement("tr")
+new_row.classList.add("table-active")
+new_row.innerHTML =
+`<th scope='row'>${element.carnet}</th>
+<td>${element.horario}</td>
+<td>${element.ingreso}</td>
+<td>${element.tarde}</td>`
+
+let new_cell = document.createElement("td")
+let new_btn= document.createElement("button")
 
 /*
-    Función para agregar un hijo a la tabla
+* Personalización del botón
 */
 
-let printArray = () => {
-    table_body.innerHTML =""
+new_btn.className = "btn btn-danger"
+new_btn.innerText = "!#$%&?"
+new_btn.value = element.id
+new_btn.disabled = true
 
-    student_list.forEach(elem =>{
-        let new_row = document.createElement("tr")
-        let datetime = new Date()
-    
-        let new_cell = document.createElement("td")
-        let new_btn = document.createElement("bttun")
-        //let new_text = document.createElement("text")
+let new_cell2 = document.createElement("td")
+let new_input= document.createElement("input")
+new_input.className = "confirmar"
+new_input.innerText = "carnet"
 
-    
-        new_row.classList.add("table-active")
-        new_row.innerHTML = 
-            `<th scope='row'>${elem.carnet}</th>
-            <td>${elem.horario}</td>
-            <td>${datetime.toLocaleString()}</td>
-            <td>${elem.tarde}</td>`
 
-        /*personalizacion del boton*/
+new_input.addEventListener("keyup", event=>{
+    let keyCode2 = event.keyCode
+    let carnet2 = element.carnet
+    let id_actual = event.target.value
 
-        new_btn.className =" btn btn-danger"
-        new_btn.innerText ="Drop"
-        new_btn.value = elem.id
-
-        //new_input.className = "form-control"
-        //new_text.innerText ="carnet"
-        //new_text.value = elem.id
-
-        new_btn.addEventListener("click", event=>{
-            let id_actual = event.target.value
-
-            student_list.forEach((elemt, pos)=>{
-                if (id_actual == elemt.id){
-                    student_list.splice(pos, 1)
-                    printArray()
-                }
-            })
-
-        })
+    if(keyCode2 == 13){
+        new_btn.click()
+        }
         
-        new_cell.appendChild(new_btn)
-        //new_text.appendChild(new_text)
-        new_row.appendChild(new_cell)
-        table_body.appendChild(new_row)
-
-    })
-}
-
-let addStudent = (carnet, schedule, late)=>{
-
-    student_list.push({
-        "id":serial,
-        "carnet": carnet,
-        "horario": schedule,
-        "tarde":late,
-    })
-
-    serial++
-}
-
-let parseLateSwitch= (value)=>{
-    if(value){
-        return "Tarde"
-    }
-    return "A tiempo"
-}
-
-submit_btn.addEventListener("click", ()=>{
-    let carnet = carnet_field.value
-    let schedule = schedule_dropdown.options[schedule_dropdown.selectedIndex].text
-    let late = parseLateSwitch(late_switch.checked)
-
-    if(carnet_regex.test(carnet)){
-        addStudent(carnet, schedule, late)
-        printArray()
+    if(carnet_regex.test(new_input.value) && new_input.value == element.carnet){
+    new_btn.disabled = false;
     }else{
-        alert("Formáto de carnet no válido")
+    new_btn.disabled = true;
     }
 })
 
+new_btn.addEventListener("click", event =>{
+let id_actual = event.target.value
+
+student_list.forEach((element, pos)=>{
+if(new_input.value == element.carnet && id_actual == element.id){
+student_list.splice(pos,1)
+printArray()
+}
+})
+})
+
+new_cell.appendChild(new_btn)
+new_cell2.appendChild(new_input)
+new_row.appendChild(new_cell)
+new_row.appendChild(new_cell2)
+table_body.appendChild(new_row)
+})
+}
+
+/*
+Función para agregar un hijo a la tabla
+*/
+
+let addStudent = (carnet, schedule, late)=>{
+let datetime = new Date()
+let datetime_string = datetime.toLocaleString()
+
+student_list.push({
+"id": serial,
+"carnet": carnet,
+"horario": schedule,
+"tarde": late,
+"ingreso": datetime_string
+})
+serial++
+}
+
+/*
+Función para parsear el valor booleano del late_switch
+*/
+
+let parseLateSwitch= (value)=>{
+if(value){
+return "Tardisimo"
+}
+return "A tiempo"
+}
+
+/*
+Listener para detectar el click en el boton
+*/
+
+submit_btn.addEventListener("click", ()=>{
+let carnet = carnet_field.value
+let schedule = schedule_dropdown.options[schedule_dropdown.selectedIndex].text
+let late = parseLateSwitch(late_switch.checked)
+
+if(carnet_regex.test(carnet)){
+addStudent(carnet, schedule, late)
+printArray()
+}else{
+alert("Formato de carnet no válido")
+}
+})
+
+/*
+Listener para disparar el botón cuando se aprete enter
+*/
+
 carnet_field.addEventListener("keyup", (event)=>{
-    let keyCode = event.keyCode
-    let carnet = carnet_field.value
+let keyCode = event.keyCode
+let carnet = carnet_field.value
 
-    if(keyCode == 13){
-        submit_btn.click()
-    }
+if(keyCode == 13){
+submit_btn.click()
+}
 
-    if(carnet_regex.test(carnet)){
-    submit_btn.disabled = false; 
-    }else{
-        submit_btn.disabled = true; 
-    }
+if(carnet_regex.test(carnet)){
+submit_btn.disabled = false;
+}else{
+submit_btn.disabled = true;
+}
 })
