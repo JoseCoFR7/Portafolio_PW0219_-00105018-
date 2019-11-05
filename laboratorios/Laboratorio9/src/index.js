5259c0e40f365f5b1229a78ce896320cac1b02aa
JoseCoFR7 <00105018@uca.edu.sv>
2019-11-04 23:40:03 -0600

    [Labo 9] Finalizado

--- laboratorios/Laboratorio9/src/index.js
+++ laboratorios/Laboratorio9/src/index.js
@@ -1,56 +1,191 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import StudentForm from './student/StudentForm';
-import StudentsList from './student/StudentsList';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import './index.css';
+var uniqueKey = 0;
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+class Tabla extends React.Component {
+  state = {
+    rows: []
+  };
 
-        this.state = {
-            students: [],
-            err: ''
-        }
-    }
+  addRow = chData => {
+    this.setState({
+      rows: this.state.rows.concat([
+        { carnet: chData.carnet, schedule: chData.schedule, late: chData.late }
+      ])
+    });
+    uniqueKey++;
+  };
 
-    saveStudent(student) {
-        const students = this.state.students.slice();
-        if (!students.find((current) => {
-            return current.carnet === student.carnet;
-        })) {
-            students.push(student);
-            this.setState({ students, err: '' });
-        } else {
-            this.setState({ err: "El estudiante ya existe" })
-        }
+  handleDeleteRow(i) {
+    let rows = [...this.state.rows];
+    rows.splice(i, 1);
+    this.setState({
+      rows: rows
+    });
+  }
 
-    }
+  renderRows() {
+    return this.state.rows.map(row => {
+      console.log(row);
+      return (
+        <tr key={uniqueKey}>
+          <td>{row.carnet}</td>
+          <td>{row.schedule}</td>
+          <td>{new Date().toLocaleDateString()}</td>
+          <td>{row.late}</td>
+          <td>
+            <button
+              value={uniqueKey}
+              className="btn"
+              className="btn-danger"
+              onClick={() => this.handleDeleteRow()}
+            >
+              Drop
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  }
 
-    deleteStudent(carnet) {
-        const students = this.state.students.filter(function (ele) {
-            return ele.carnet !== carnet;
-        });
-        this.setState({ students });
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <div>{this.state.err}</div>
-                    <StudentForm onSave={(student) => {
-                        this.saveStudent(student)
-                    }} />
-                </div>
-                <StudentsList students={this.state.students} onDelete={(carnet) => {
-                    this.deleteStudent(carnet);
-                }} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <Form callBackFromParent={this.addRow} />
+        <section>
+          <table className="table table-hover">
+            <thead>
+              <tr className="table-dark">
+                <th scope="col">Carnet</th>
+                <th scope="col">Horario de laboratorio</th>
+                <th scope="col">Hora de ingreso</th>
+                <th scope="col">¿Tarde?</th>
+                <th scope="col">Eliminar</th>
+              </tr>
+            </thead>
+            <tbody id="table_body">{this.renderRows()}</tbody>
+          </table>
+        </section>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  renderSelect(message) {
+    return (
+      <div className="form-group">
+        <label htmlFor="schedule">Seleccione el horario:</label>
+        <select name="schedule" className="form-control" id="schedule_field">
+          <option>{message[0]}</option>
+          <option>{message[1]}</option>
+          <option>{message[2]}</option>
+          <option>{message[3]}</option>
+          <option>{message[4]}</option>
+          <option>{message[5]}</option>
+        </select>
+      </div>
+    );
+  }
+
+  renderInput() {
+    return (
+      <div className="form-group">
+        <label htmlFor="carnet" className="col-sm-2 col-form-label">
+          Ingrese el carnet:{" "}
+        </label>
+        <input
+          className="form-control"
+          type="text"
+          name="carnet"
+          id="carnet_field"
+        />
+      </div>
+    );
+  }
+
+  renderH1(message) {
+    return <h1>{message}</h1>;
+  }
+
+  renderSwitch() {
+    return (
+      <div className="form-group">
+        <div className="custom-control custom-switch">
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="late_switch"
+          />
+          <label className="custom-control-label" htmlFor="late_switch">
+            ¿Llegó tarde?
+          </label>
+        </div>
+      </div>
+    );
+  }
+
+  renderInsertBtn() {
+    return (
+      <div className="form-group">
+        <button
+          type="button"
+          className="btn btn-danger"
+          id="submit_btn"
+          onClick={() => this.handleClick()}
+        >
+          Ingresar
+        </button>
+      </div>
+    );
+  }
+
+  handleClick = () => {
+    let carnet = document.getElementById("carnet_field").value;
+    let schedule = document.getElementById("schedule_field").options;
+    let realSchedule = schedule[schedule.selectedIndex].text;
+    let late = parseLateSwitch(document.getElementById("late_switch").checked);
+    this.props.callBackFromParent({
+      carnet: carnet,
+      schedule: realSchedule,
+      late: late
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          {this.renderH1("Registro de laboratorio")}
+          {this.renderInput()}
+          {this.renderSelect([
+            "Lunes de 9:00 a 11.00",
+            "Martes de 13:30 a 15:30",
+            "Miércoles de 9:00 a 11.00",
+            "Jueves de 13:30 a 15:30",
+            "Viernes de 9:00 a 11.00",
+            "Viernes de 15:30 a 17:30"
+          ])}
+          {this.renderSwitch()}
+          {this.renderInsertBtn()}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Tabla />, document.getElementById("root"));
+
+var parseLateSwitch = value => {
+  if (value) {
+    return "LLego Tarde";
+  } else {
+    return "LLego temprano";
+  }
+};
